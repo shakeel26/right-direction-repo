191ee1b3d198d2e02a336c7dda0883c69eff72f5
jay.mathins <jay.mathins@treelineinteractive.com>
2021-11-24 16:17:00 +0500

    add shop page images

--- src/pages/shope/Shop.jsx
+++ src/pages/shope/Shop.jsx
@@ -5,7 +5,7 @@ function Shop() {
   return (
     <div>
       <div className="shop-first-image-box">
-        <img className="shop-first-image" src="/images/outfit-image/0M6A7695.jpg" alt="img" />
+        <img className="shop-first-image" src="/images/shop-page/shop-img-1.png" alt="img" />
         <h5>
           Right D<span className="c-white">irection</span>
         </h5>
@@ -13,10 +13,10 @@ function Shop() {
       {/* second section */}
       <div className="second-section">
         <div className="sec-image-left">
-          <img className="sec-sect-image" src="/images/outfit-image/0M6A7695.jpg" alt="img" />
+          <img className="sec-sect-image" src="/images/shop-page/shop-img-2.png" alt="img" />
         </div>
         <div className="sec-image-right">
-          <img className="right-side-image" src="/images/outfit-image/0M6A7695.jpg" alt="img" />
+          <img className="right-side-image" src="/images/shop-page/shop-img-3.png" alt="img" />
           <h5 className="second-sec-title">
             co-or
             <span className="c-white">
@@ -31,10 +31,10 @@ function Shop() {
           <span className="c-white">Coats</span> & Jackets
         </h5>
         <div className="third-sec-img-div">
-          <img className="third-sec-img" src="/images/outfit-image/0M6A7695.jpg" alt="img" />
+          <img className="third-sec-img" src="/images/shop-page/shop-img-4.png" alt="img" />
         </div>
         <div className="third-sec-img-div">
-          <img className="third-sec-img" src="/images/outfit-image/0M6A7695.jpg" alt="img" />
+          <img className="third-sec-img" src="/images/shop-page/shop-img-5.png" alt="img" />
         </div>
       </div>
     </div>
